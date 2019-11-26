5b8b62f7ff769c335e8d476ef27eb3ade4b9a380
nakamarusun <nakamarujc@gmail.com>
2019-11-26 10:11:31 +0700

    selesai no 1

--- AssortedProblems.py
+++ AssortedProblems.py
@@ -1,1 +1,13 @@
 #1
+sampleData = "3, 5, 7, 23"
+sampleData = sampleData.replace(",", "") # "3 5 7 23" string
+newArr = sampleData.split() # [3,5,7,23] list
+print(newArr)
+print(tuple(newArr))
+
+#2
+translated = "this is NOT fun"
+def translateGaje(stnc):
+    #jojoi
+
+translated.
