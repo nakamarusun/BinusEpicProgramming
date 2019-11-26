06c20263766b930251fe322beaafe614d2c36335
nakamarusun <nakamarujc@gmail.com>
2019-11-26 10:55:47 +0700

    finished AssortedProblems Challenge

--- AssortedProblems.py
+++ AssortedProblems.py
@@ -6,8 +6,43 @@ print(newArr)
 print(tuple(newArr))
 
 #2
-translated = "this is NOT fun"
-def translateGaje(stnc):
-    #jojoi
+def translateGaje(stnc: str):
+    vowel = "aiueo "
+    stnc = list(stnc)
+    for i in stnc:
+        if i not in vowel:
+            stnc[stnc.index(i)] = i + 'o' + i
+    return(''.join(stnc))
 
-translated.
+print(translateGaje("this is fun"))
+
+#4
+def isMember(char: str, list: [str]):
+    for i in list:
+        if char == i:
+            return True
+    return False
+
+#5
+def overlapping(list1: [str], list2: [str]):
+    for i in list1:
+        for j in list2:
+            if i == j:
+                return True
+    return False
+
+#6
+def histogram(list: [int]):
+    for i in list:
+        print("*" * i)
+
+histogram([5,3,6])
+
+#7
+def stringListToInt(list: [str]):
+    intList: [int] = []
+    for i in list:
+        intList.append(len(i))
+    return intList
+
+print(stringListToInt(["aaaaa","bbbb"]))
