999176818c66718cf2b204b5def62ebb69f7b754
nakamarusun <48478366+nakamarusun@users.noreply.github.com>
2019-12-02 22:06:16 +0700

    modified test2

--- test2.py
+++ test2.py
@@ -1,15 +1,15 @@
 def prime(n):
-    prime = [2]
-    testPrime = 1
+    prime, testPrime = [2], 1
     while len(prime) < n:
         primeCheck = False
         testPrime += 2
         for i in prime:
             if (testPrime % i == 0):
                 primeCheck = True
+                break
         if primeCheck == False:
             prime.append(testPrime)
     print(prime)
     return prime[n-1]
 
-print(prime(10))
+print(prime(100000))
